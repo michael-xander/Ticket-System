90ecf599652e57b2e9c4b03c688e4dd1d9dbf76e
Michael <michaelkyeyune01@gmail.com>
2015-08-07 11:43:07 +0200

    Creating place holder classes for the packages in the repo - Set up LoginServlet.java for the controllers/servlets package - Set up UserDao.java for the data/access package - Set up PersonDaoUnitTest.java for the test/java/data/access package

---
+++ src/main/java/controllers/servlets/LoginServlet.java
@@ -0,0 +1,9 @@
+package controllers.servlets;
+
+/**
+ * LoginServlet.java
+ * A Servlet that handles logging into the TicketSystem
+ * Created by Michael on 2015/08/07.
+ */
+public class LoginServlet {
+}
---
+++ src/test/java/data/access/PersonDaoUnitTest.java
@@ -0,0 +1,9 @@
+package data.access;
+
+/**
+ * PersonDaoUnitTest.java
+ * A class that tests the functionality of the PersonDaoImpl class
+ * Created by Michael on 2015/08/07.
+ */
+public class PersonDaoUnitTest {
+}
