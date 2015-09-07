fc2f10220cd9dbd632b6cc9a3cf7619ecd8ba84a
Michael <michaelkyeyune01@gmail.com>
2015-09-08 01:49:02 +0200

    Made filter for faq table views functional

--- src/main/java/view/dashboard/UserFaqTableView.java
+++ src/main/java/view/dashboard/UserFaqTableView.java
@@ -1,5 +1,8 @@
 package view.dashboard;
 
+import com.vaadin.data.Container;
+import com.vaadin.data.Item;
+import com.vaadin.event.FieldEvents;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Table;
@@ -22,7 +25,7 @@ public abstract class UserFaqTableView extends UserTableView {
         table.addStyleName(ValoTheme.TABLE_COMPACT);
         table.setSelectable(true);
 
-        table.addContainerProperty("Frequently Asked Questions", String.class, "(default)");
+        table.addContainerProperty("Question", String.class, "(default)");
 
         table.setImmediate(true);
 
@@ -32,6 +35,32 @@ public abstract class UserFaqTableView extends UserTableView {
     @Override
     public Component buildFilter() {
         final TextField filter = new TextField();
+        //adding the table filter
+        filter.addTextChangeListener(new FieldEvents.TextChangeListener() {
+            @Override
+            public void textChange(FieldEvents.TextChangeEvent textChangeEvent) {
+                Container.Filterable data = (Container.Filterable) getTable().getContainerDataSource();
+                data.removeAllContainerFilters();
+                data.addContainerFilter(new Container.Filter() {
+                    @Override
+                    public boolean passesFilter(Object o, Item item) throws UnsupportedOperationException {
+                        if(textChangeEvent.getText() == null || textChangeEvent.getText().equals(""))
+                            return true;
+
+                        return filterByProperty("Question", item, textChangeEvent.getText());
+                    }
+
+                    @Override
+                    public boolean appliesToProperty(Object propertyId) {
+                        if(propertyId.equals("Question"))
+                            return true;
+
+                        return false;
+                    }
+                });
+            }
+        });
+
         filter.setInputPrompt("Filter");
         filter.setIcon(FontAwesome.SEARCH);
         filter.addStyleName(ValoTheme.TEXTFIELD_INLINE_ICON);
