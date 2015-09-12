1bb16995273ea42f63790a63c781e376a9ac1c8e
Michael <michaelkyeyune01@gmail.com>
2015-09-12 17:44:16 +0200

    Split functionality of default query view and course query view for administrator

--- src/main/java/view/dashboard/convener/views/AdminCourseQueryView.java
+++ src/main/java/view/dashboard/convener/views/AdminCourseQueryView.java
@@ -1,6 +1,12 @@
 package view.dashboard.convener.views;
 
 
+import com.vaadin.server.Responsive;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.themes.ValoTheme;
 import model.domain.message.Query;
 import view.TicketSystemUI;
 
@@ -25,6 +31,36 @@ public class AdminCourseQueryView extends AdminQueryTableView
 
         addComponent(super.getTable());
         setExpandRatio(super.getTable(), 1);
+
+        super.getTable().setMultiSelect(true);
+    }
+
+    @Override
+    public Component buildToolbar(String toolbarHeader) {
+        HorizontalLayout header = new HorizontalLayout();
+        header.addStyleName("viewheader");
+        header.setSpacing(true);
+        Responsive.makeResponsive(header);
+
+        Label title = new Label(toolbarHeader);
+        title.setSizeUndefined();
+        title.addStyleName(ValoTheme.LABEL_H1);
+        title.addStyleName(ValoTheme.LABEL_NO_MARGIN);
+        header.addComponent(title);
+
+        Component filter = buildFilter();
+        Component createReply = buildCreateReply();
+        HorizontalLayout tools = new HorizontalLayout(filter, createReply);
+        tools.setSpacing(true);
+        tools.addStyleName("toolbar");
+        header.addComponent(tools);
+        return header;
+    }
+
+    public Button buildCreateReply()
+    {
+        final Button createReply = new Button("Create Reply");
+        return createReply;
     }
 
     @Override
